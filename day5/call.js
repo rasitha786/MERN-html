5db5c63d346bfe55842552d463a0ff459904f794
rasitha <rasithar@gmail.com>
2025-12-05 09:47:58 +0530

    rr

--- day5/call.js
+++ day5/call.js
@@ -1,6 +1,39 @@
-console.log("first");
+/* console.log("first");
 setTimeout(()=> {
     console.log("this run after");
     
 },2000)
-console.log("end");
+console.log("end"); */
+
+/* setTimeout(() => {
+    console.log("made tea");
+    
+    setTimeout(()=> {
+        console.log("toasted bread");
+
+        setTimeout(() => {
+            console.log("fried egg");
+            
+        },5000);
+        
+    },5000);
+},5000); */
+
+/* let iceCreamPromise = new Promise((resolve,reject)=> {
+    let iceCreamReady = false;
+
+    if(iceCreamReady) {
+        resolve("ice cream ready!");
+    } else {
+        reject("no ice cream today")
+    }
+});
+iceCreamPromise
+.then((message)=> {
+    console.log("yayy!!"+message);
+    
+})
+.catch((error)=>{
+    console.log("Oops!!"+error);
+    
+}); */
